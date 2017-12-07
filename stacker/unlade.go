13129c8965fb9a142d2628c9ccab905f47e056d1
Tycho Andersen <tycho@tycho.ws>
2017-12-07 23:36:08 +0000

    stacker unlade
    Signed-off-by: Tycho Andersen <tycho@tycho.ws>

---
+++ stacker/unlade.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/anuvu/stacker"
+	"github.com/openSUSE/umoci"
+	"github.com/urfave/cli"
+)
+
+var unladeCmd = cli.Command{
+	Name: "unlade",
+	Usage: "unpacks an OCI image to a directory",
+	Action: doUnlade,
+	Flags: []cli.Flag{},
+}
+
+func doUnlade(ctx *cli.Context) error {
+	file := ctx.String("f")
+	sf, err := stacker.NewStackerfile(file)
+	if err != nil {
+		return err
+	}
+
+	s, err := stacker.NewStorage(config)
+	if err != nil {
+		return err
+	}
+
+	oci, err := umoci.OpenLayout(config.OCIDir)
+	if err != nil {
+		return err
+	}
+
+	tags, err := oci.ListTags()
+	if err != nil {
+		return err
+	}
+
+	for _, tag := range tags {
+		
+	}
+}
