da2456b34da99939ba41f26447b626e65ac40c69
Tycho Andersen <tycho@tycho.ws>
2019-04-01 11:10:14 -0600

    implement per-layer copy on write snapshots
    The idea here is that we will be able to start from an existing snapshot
    instead of re-extracting everything.
    
    Signed-off-by: Tycho Andersen <tycho@tycho.ws>

--- cmd/umoci.go
+++ cmd/umoci.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
+	"path"
+	"strings"
 	"time"
 
+	"github.com/anuvu/stacker"
 	"github.com/openSUSE/umoci"
 	"github.com/openSUSE/umoci/mutate"
 	"github.com/openSUSE/umoci/oci/casext"
 	"github.com/openSUSE/umoci/oci/layer"
+	"github.com/openSUSE/umoci/pkg/fseval"
 	ispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/pkg/errors"
 	"github.com/urfave/cli"
@@ -67,7 +72,7 @@ func doInit(ctx *cli.Context) error {
 	}
 
 	opts := layer.MapOptions{KeepDirlinks: true}
-	err = umoci.Unpack(oci, name, bundlePath, opts)
+	err = umoci.Unpack(oci, name, bundlePath, opts, nil, ispec.Descriptor{})
 	if err != nil {
 		return errors.Wrapf(err, "umoci unpack failed for %s into %s", name, bundlePath)
 	}
@@ -81,8 +86,124 @@ func doUnpack(ctx *cli.Context) error {
 		return err
 	}
 
+	storage, err := stacker.NewStorage(config)
+	if err != nil {
+		return err
+	}
+
+	tag := ctx.GlobalString("tag")
+	bundlePath := ctx.GlobalString("bundle-path")
+
+	manifest, err := stacker.LookupManifest(oci, tag)
+	if err != nil {
+		return err
+	}
+
+	startFrom := ispec.Descriptor{}
+	highestHash := ""
+	for _, layerDesc := range manifest.Layers {
+		hash, err := stacker.ComputeAggregateHash(manifest, layerDesc)
+		if err != nil {
+			return err
+		}
+
+		if storage.Exists(hash) {
+			highestHash = hash
+			startFrom = layerDesc
+			fmt.Println("found previous extraction of", layerDesc.Digest.String())
+		} else {
+			break
+		}
+	}
+
+	if highestHash != "" {
+		// Delete the previously created working snapshot; we're about
+		// to create a new one.
+		err = storage.Delete(".working")
+		if err != nil {
+			return err
+		}
+
+		// TODO: this is a little wonky: we're assuming that
+		// bundle-path ends in .working. It always does because
+		// this is an internal API, but we should refactor this
+		// a bit.
+		err = storage.Restore(highestHash, ".working")
+		if err != nil {
+			return err
+		}
+
+		// We need the mtree metadata to be present, but since these
+		// intermediate snapshots were created after each layer was
+		// extracted and the metadata wasn't, it won't necessarily
+		// exist. We could create it at extract time, but that would
+		// make everything really slow, since we'd have to walk the
+		// whole FS after every layer which would probably slow things
+		// way down.
+		//
+		// Instead, check to see if the metadata has been generated. If
+		// it hasn't, we generate it, and then re-snapshot back (since
+		// we can't write to the old snapshots) with the metadata.
+		//
+		// This means the first restore will be slower, but after that
+		// it will be very fast.
+		_, err := os.Stat(path.Join(bundlePath, "config.json"))
+		if err != nil {
+			fmt.Println("generating mtree metadata for snapshot (this may take a bit)...")
+			dps, err := oci.ResolveReference(context.Background(), tag)
+			if err != nil {
+				return err
+			}
+
+			meta := umoci.Meta{
+				Version:    umoci.MetaVersion,
+				MapOptions: layer.MapOptions{},
+				From:       dps[0],
+			}
+
+			// generate the metadata
+			mtreeName := strings.Replace(meta.From.Descriptor().Digest.String(), ":", "_", 1)
+			err = umoci.GenerateBundleManifest(mtreeName, bundlePath, fseval.DefaultFsEval)
+			if err != nil {
+				return err
+			}
+
+			err = umoci.WriteBundleMeta(bundlePath, meta)
+			if err != nil {
+				return err
+			}
+
+			err = storage.Delete(highestHash)
+			if err != nil {
+				return err
+			}
+
+			err = storage.Snapshot(".working", highestHash)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	// If we restored from the last extracted layer, we don't need to do
+	// anything, and can just return.
+	if startFrom.Digest.String() == manifest.Layers[len(manifest.Layers)-1].Digest.String() {
+		return nil
+	}
+
+	// TODO: we could always share the empty layer, but that's more code
+	// and seems extreme...
+	callback := func(manifest ispec.Manifest, desc ispec.Descriptor) error {
+		hash, err := stacker.ComputeAggregateHash(manifest, desc)
+		if err != nil {
+			return err
+		}
+
+		return storage.Snapshot(".working", hash)
+	}
+
 	opts := layer.MapOptions{KeepDirlinks: true}
-	return umoci.Unpack(oci, ctx.GlobalString("tag"), ctx.GlobalString("bundle-path"), opts)
+	return umoci.Unpack(oci, ctx.GlobalString("tag"), bundlePath, opts, callback, startFrom)
 }
 
 func doRepack(ctx *cli.Context) error {
--- go.mod
+++ go.mod
@@ -27,7 +27,7 @@ require (
 	github.com/flosch/pongo2 v0.0.0-20181225140029-79872a7b2769
 	github.com/freddierice/go-losetup v0.0.0-20170407175016-fc9adea44124
 	github.com/ghodss/yaml v0.0.0-20190206175653-d4115522f0fe
-	github.com/golang/protobuf v1.3.0
+	github.com/golang/protobuf v1.3.1
 	github.com/gorilla/mux v1.7.0
 	github.com/gorilla/websocket v0.0.0-20190205004414-7c8e298727d1
 	github.com/hashicorp/errwrap v1.0.0
@@ -36,26 +36,30 @@ require (
 	github.com/klauspost/compress v1.4.1
 	github.com/klauspost/cpuid v1.2.0
 	github.com/klauspost/pgzip v1.2.1
-	github.com/konsorten/go-windows-terminal-sequences v1.0.1
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2
 	github.com/lxc/lxd v0.0.0-20190208124523-fe0844d45b32
+	github.com/mattn/go-colorable v0.1.1 // indirect
+	github.com/mattn/go-isatty v0.0.7 // indirect
 	github.com/mattn/go-runewidth v0.0.0-20181218000649-703b5e6b11ae
 	github.com/matttproud/golang_protobuf_extensions v1.0.1
 	github.com/mitchellh/hashstructure v1.0.0
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/mtrmac/gpgme v0.0.0-20170102180018-b2432428689c
-	github.com/openSUSE/umoci v0.0.0-20190209041853-4ad066106584
+	github.com/openSUSE/umoci v0.4.4
 	github.com/opencontainers/go-digest v1.0.0-rc1
-	github.com/opencontainers/image-spec v1.0.0
+	github.com/opencontainers/image-spec v1.0.1
 	github.com/opencontainers/runc v0.0.0-20190208075259-dd023c457d84
-	github.com/opencontainers/runtime-spec v1.0.0
+	github.com/opencontainers/runtime-spec v1.0.1
 	github.com/opencontainers/runtime-tools v0.7.0
-	github.com/pkg/errors v0.8.0
+	github.com/opencontainers/selinux v1.0.0 // indirect
+	github.com/pkg/errors v0.8.1
 	github.com/prometheus/client_golang v0.9.1
 	github.com/prometheus/client_model v0.0.0-20190129233127-fd36f4220a90
 	github.com/prometheus/common v0.2.0
 	github.com/prometheus/procfs v0.0.0-20190208162519-de1b801bf34b
 	github.com/rootless-containers/proto v0.1.0
 	github.com/sergi/go-diff v0.0.0-20180205163309-da645544ed44
-	github.com/sirupsen/logrus v1.3.0
+	github.com/sirupsen/logrus v1.4.0
 	github.com/syndtr/gocapability v0.0.0-20180916011248-d98352740cb2
 	github.com/udhos/equalfile v0.3.0
 	github.com/ulikunitz/xz v0.5.5
@@ -63,11 +67,12 @@ require (
 	github.com/vbatts/go-mtree v0.4.4
 	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415
-	github.com/xeipuuv/gojsonschema v0.0.0-20180719132039-b84684d0e066
-	golang.org/x/crypto v0.0.0-20190208162236-193df9c0f06f
-	golang.org/x/net v0.0.0-20190206173232-65e2d4e15006
+	github.com/xeipuuv/gojsonschema v1.1.0
+	golang.org/x/crypto v0.0.0-20190325154230-a5d413f7728c
+	golang.org/x/net v0.0.0-20190327091125-710a502c58a2
 	golang.org/x/sync v0.0.0-20181221193216-37e7f081c4d4
-	golang.org/x/sys v0.0.0-20190204203706-41f3e6584952
+	golang.org/x/sys v0.0.0-20190322080309-f49334f85ddc
+	google.golang.org/genproto v0.0.0-20180831171423-11092d34479b // indirect
 	gopkg.in/cheggaaa/pb.v1 v1.0.27
 	gopkg.in/lxc/go-lxc.v2 v2.0.0-20181227225324-7c910f8a5edc
 	gopkg.in/robfig/cron.v2 v2.0.0-20150107220207-be2e0b0deed5
@@ -76,4 +81,4 @@ require (
 
 replace github.com/vbatts/go-mtree v0.4.4 => github.com/vbatts/go-mtree v0.4.5-0.20190122034725-8b6de6073c1a
 
-replace github.com/openSUSE/umoci v0.0.0-20190209041853-4ad066106584 => github.com/tych0/umoci v0.0.0-20190209041853-4ad066106584
+replace github.com/openSUSE/umoci v0.4.4 => github.com/tych0/umoci v0.1.1-0.20190401143912-9989a6c702b5
